48e455503ed6ad04ada4281a637a258d5f743913
santhosh210 <kondasanthosh210@gmail.com>
2024-03-15 18:22:10 +0530

    added topoldy to db uri

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 module.exports = function () {
   mongoose
     // .connect("mongodb://localhost:27017/vidly-test")
-    .connect("mongodb://0.0.0.0:27017/vidly")
+
+    .connect(
+      "mongodb://0.0.0.0:27017/vidly",
+      { useUnifiedTopology: true },
+      { useNewUrlParser: true },
+      { connectTimeoutMS: 30000 },
+      { keepAlive: 1 }
+    )
     // .then(() => logger.info("Connected to database"));
     .then("connected to db");
 };
