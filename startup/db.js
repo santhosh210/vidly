8d281a6fbce492a360141b23b518bca7e8bc33ec
santhosh210 <kondasanthosh210@gmail.com>
2024-03-16 01:32:08 +0530

    removed magority from db uri

--- startup/db.js
+++ startup/db.js
@@ -1,27 +1,14 @@
-// // const logger = require("./winston");
-// const mongoose = require("mongoose");
-// module.exports = function () {
-//   mongoose
-//     // .connect("mongodb://localhost:27017/vidly-test")
-
-//     .connect(
-//       "mongodb://0.0.0.0:27017/vidly",
-//       { useUnifiedTopology: true },
-//       { useNewUrlParser: true },
-//       { connectTimeoutMS: 30000 },
-//       { keepAlive: 1 }
-//     )
-//     // .then(() => logger.info("Connected to database"));
-//     .then("connected to db");
-// };
 const mongoose = require("mongoose");
 
 module.exports = function () {
   mongoose
     .connect(
-      "mongodb+srv://kondasanthosh210:12345@vidly.cffparb.mongodb.net/?retryWrites=true/vidly",
+      "mongodb+srv://kondasanthosh210:12345@vidly.cffparb.mongodb.net/vidly",
       {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
         connectTimeoutMS: 30000,
+        keepAlive: true, // Keep the connection alive
       }
     )
     .then(() => console.log("Connected to database"))
