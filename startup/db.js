fa60160773735809271331fee20257003e45da6d
santhosh210 <kondasanthosh210@gmail.com>
2024-03-15 18:35:59 +0530

    made some changes

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,26 @@
-// const logger = require("./winston");
+// // const logger = require("./winston");
+// const mongoose = require("mongoose");
+// module.exports = function () {
+//   mongoose
+//     // .connect("mongodb://localhost:27017/vidly-test")
+
+//     .connect(
+//       "mongodb://0.0.0.0:27017/vidly",
+//       { useUnifiedTopology: true },
+//       { useNewUrlParser: true },
+//       { connectTimeoutMS: 30000 },
+//       { keepAlive: 1 }
+//     )
+//     // .then(() => logger.info("Connected to database"));
+//     .then("connected to db");
+// };
 const mongoose = require("mongoose");
+
 module.exports = function () {
   mongoose
-    // .connect("mongodb://localhost:27017/vidly-test")
-
-    .connect(
-      "mongodb://0.0.0.0:27017/vidly",
-      { useUnifiedTopology: true },
-      { useNewUrlParser: true },
-      { connectTimeoutMS: 30000 },
-      { keepAlive: 1 }
-    )
-    // .then(() => logger.info("Connected to database"));
-    .then("connected to db");
+    .connect("mongodb://localhost:27017/vidly", {
+      connectTimeoutMS: 30000,
+    })
+    .then(() => console.log("Connected to database"))
+    .catch((error) => console.error("Error connecting to database:", error));
 };
